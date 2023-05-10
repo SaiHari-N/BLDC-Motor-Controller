6baf0df3858878e4ae580151f0c432e562aa7c74
SaiHari_N <74491747+SaiHari-N@users.noreply.github.com>
2023-05-10 11:34:17 +0530

    Add files via upload

---
+++ Atmel Studio Project/Inc/hall_sensor.h
@@ -0,0 +1,15 @@
+// Hall Commuttaion States mapped to respective PWM Outputs
+#define LS_U 	0x01     	//TB1
+#define HS_U 	0x02     	//TB2
+#define LS_V 	0x04     	//TB3
+#define HS_V 	0x08     	//TB4
+#define LS_W 	0x10     	//TB5
+#define HS_W 	0x20     	//TB6
+
+// Motor Direction Defintions
+#define CW 	0x01		// clockwise direction 
+#define CCW 	0x00		// counter clockwise direction
+
+
+
+
